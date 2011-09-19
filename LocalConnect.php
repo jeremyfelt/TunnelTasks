15192774c8b6efbf393b54bf504a5137082c09e7
Jeremy Felt <jeremy.felt@gmail.com>
2011-09-19 19:20:47 +0000

    Adding possibilities

---
+++ LocalConnect.php
@@ -0,0 +1,22 @@
+<?php
+
+class LocalConnect{
+
+    public function __construct(){
+
+
+    }
+
+    public function run_on_local($cmd_sequence){
+
+
+
+    }
+
+    public function __destruct(){
+
+    }
+
+}
+
+?>
