33da3e16521f24f48249c36fcfa973f34b9afca5
Jeremy Felt <jeremy.felt@gmail.com>
2011-09-19 20:11:18 +0000

    First temporary confirmation of additional sql script

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 
 require ( 'config.php' );
 require ( 'TunnelConnect.php' );
+require ( 'LocalConnect.php' );
 
 /*  Build the file we want to pass back and forth. */
 $file_date = date('YmdHi');
@@ -15,11 +16,20 @@ $remote_command = 'cd ' . $production_mysql_dir . ';'
                         . 'mysqldump -u ' . $production_mysql_user . ' -p' . $production_mysql_pass
                         . ' ' . $production_mysql_db . ' > ' . $production_mysql_dir . $production_mysql_file . ';';
 
+$local_command = 'cd ' . $local_mysql_dir . ';'
+                . 'mysql -u ' . $local_mysql_user . ' -p' . $local_mysql_pass
+                . ' ' . $production_mysql_db . ' < ' . $production_mysql_file . ';'
+                . 'mysql -u ' . $local_mysql_user . ' -p' . $local_mysql_pass
+                . ' ' . $production_mysql_db . ' < test_inserts.sql';
 
 $my_tunnel = new TunnelConnect($production_ssh_ip, $production_ssh_port, $production_ssh_user, $production_ssh_pass);
 
 $my_tunnel->run_on_remote($remote_command);
 $my_tunnel->grab_remote_file($production_mysql_dir . $production_mysql_file, $local_mysql_dir . $production_mysql_file);
 $my_tunnel = NULL;
+
+$my_local = new LocalConnect();
+$my_local->run_on_local($local_command);
+
 
 ?>
