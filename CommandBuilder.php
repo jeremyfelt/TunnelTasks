43e0f97b4b6d677362ae1da731a161229601dd3e
Jeremy Felt <jeremy.felt@gmail.com>
2011-09-19 22:48:52 +0000

    Adding class to build commands easier

---
+++ CommandBuilder.php
@@ -0,0 +1,45 @@
+<?php
+
+class CommandBuilder {
+
+    public $command_sequence;
+
+    public function __construct(){
+
+        $this->command_sequence = '';
+
+    }
+
+    public function change_directory($new_directory){
+
+        $this->command_sequence .= 'cd ' . $new_directory . ';';
+
+    }
+
+    public function move_file($file_name, $new_directory){
+
+        $this->command_sequence .= 'mv ' . $file_name . ' ' . $new_directory . ';';
+
+    }
+
+    public function dump_database($db_name, $db_user, $db_pass, $db_file){
+
+        $this->command_sequence .= 'mysqldump -u ' . $db_user . ' -p' . $db_pass
+                                . ' ' . $db_name . ' > ' . $db_file . ';';
+
+    }
+
+    public function import_database($db_name, $db_user, $db_pass, $db_file){
+
+        $this->command_sequence .= 'mysql -u ' . $db_user . ' -p'. $db_pass
+                                . ' ' . $db_name . ' < ' . $db_file . ';';
+
+    }
+
+    public function __destruct(){
+
+        $this->command_sequence = NULL;
+
+    }
+
+}
