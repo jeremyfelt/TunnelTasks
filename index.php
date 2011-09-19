3d0b9272387c6f2182c5b7491b34d9ddf3b9964b
Jeremy Felt <jeremy.felt@gmail.com>
2011-09-19 19:14:07 +0000

    Example use of TunnelConnect function to pass commands and receive files

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 
-/*    Purposely empty until I figure out what to do with it, but it will be needed. :) */
+/*  Current example 'technically' works. */
+
+require ( 'config.php' );
+require ( 'TunnelConnect.php' );
+
+/*  Build the file we want to pass back and forth. */
+$file_date = date('YmdHi');
+$production_mysql_file = $file_date . "_" . $production_mysql_db . ".sql";
+
+/*  Build the command that we'll issue on the remote server. */
+
+$remote_command = 'cd ' . $production_mysql_dir . ';'
+                        . 'mysqldump -u ' . $production_mysql_user . ' -p' . $production_mysql_pass
+                        . ' ' . $production_mysql_db . ' > ' . $production_mysql_dir . $production_mysql_file . ';';
+
+
+$my_tunnel = new TunnelConnect($production_ssh_ip, $production_ssh_port, $production_ssh_user, $production_ssh_pass);
+
+$my_tunnel->run_on_remote($remote_command);
+$my_tunnel->grab_remote_file($production_mysql_dir . $production_mysql_file, $local_mysql_dir . $production_mysql_file);
+$my_tunnel = NULL;
 
 ?>
